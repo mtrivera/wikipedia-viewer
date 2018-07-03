cc18c5b50afc7de60429de4d740d2a4e10ec98d4
Miguel T Rivera <mrivera108@outlook.com>
2018-07-03 16:05:02 -0700

    Cleanup displayResults() and getUserInput()

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,6 @@
 function getUserInput() {
   $('button.search').on('click', function() {
-    let query = $('input').val();
-    getQuery(query);
+    getQuery($('input').val());
   });
 }
 
@@ -24,18 +23,21 @@ function getQuery(searchQuery) {
 }
 
 function displayResults(listData) {
-  let maxResults = 10;
+  const MAX_RESULTS = 10;
   let html = '';
+  
+  html += '<h2 class="text-center">Results</h2>';
 
-  for(let count = 0; count < maxResults; count++) { 
-    html += '<li>';
-    html += '<a href=' + listData[3][count] + ' ' + 'target=_blank>';
-    html += '<h2>' + listData[1][count] + '</h2>';
+  for (let count = 0; count < MAX_RESULTS; count += 1) { 
+    html += '<li class="result p-4 mb-4">';
+    html += '<a href=' + listData[3][count] + ' ' + 'target="_blank">';
+    html += '<h3>' + listData[1][count] + '</h3>';
     html += '<p>' + listData[2][count] + '</p>';
     html += '</a>';
     html += '</li>';
   }
-  $('ul.list').html(html);
+
+  $('ul.results').html(html);
 }
 
 $(document).ready(function() {
